1478b360062918cdcf9301bcb4f5fc9f80695b19
Parth Vyas <itspv7@gmail.com>
2024-10-09 21:14:00 +0530

    Finished styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Input } from "@/components/ui/input";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { Button } from "./components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { useState, useEffect,useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import { RxCross1 } from "react-icons/rx";
 import { CiSearch } from "react-icons/ci";
-import useDebounce from './hooks/useDebounce'
+import useDebounce from "./hooks/useDebounce";
+
 interface Task {
   id: string;
   content: string;
@@ -19,8 +20,10 @@ const App: React.FC = () => {
     const savedTasks = localStorage.getItem("tasks");
     return savedTasks ? JSON.parse(savedTasks) : [];
   });
+  const inputRef = useRef(null);
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
+    inputRef.current?.focus();
   }, [tasks]);
   const [task, setTask] = useState<Task>({
     id: Math.random().toString(36).slice(2, 9),
@@ -40,6 +43,7 @@ const App: React.FC = () => {
     ]);
     setTask({ id: newTaskId, content: "", complete: false });
   };
+
   const segregatedTasks = useCallback(() => {
     return tasks.filter((task) => {
       const matchesFilter =
@@ -73,8 +77,21 @@ const App: React.FC = () => {
       })
     );
   };
+  const handleAddTaskInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTask({ ...task, content: e.currentTarget.value });
+  };
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleAddTask();
+    }
+  };
   const handleDelete = (id: string) => {
-    setTasks(tasks.filter((task) => task.id !== id));
+    const userConfirmation = confirm(
+      "Are you sure you want to delete this task?"
+    );
+    if (userConfirmation) {
+      setTasks(tasks.filter((task) => task.id !== id));
+    }
   };
   const handleViewCompleted = () => setFilter("complete");
   const handleViewIncomplete = () => setFilter("incomplete");
@@ -82,25 +99,30 @@ const App: React.FC = () => {
 
   return (
     <>
-      <div className="mx-auto py-4">
-        <div className="flex flex-col md:flex-row">
-          <h1 className="text-4xl">Today</h1>
-          <div className="relative">
+      <div className="mx-auto p-4 md:px-0 md:w-3/4">
+        <div className="flex flex-col md:flex-row gap-2 md:gap-4 items-center justify-between">
+          <h1 className="text-4xl font-bold">Today</h1>
+          <div className="relative w-full">
             <Input
               type="text"
               placeholder="Search"
               value={searchTerm}
               onChange={handleSearch}
-              className="rounded-full pl-10 pr-4 py-2 w-full"
+              className="rounded-full pl-10 pr-4 py-2 w-full text-lg"
             />
             <CiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
           </div>
-          <ToggleGroup size={"lg"} type="single">
+          <ToggleGroup
+            size={"lg"}
+            type="single"
+            variant={"outline"}
+            defaultValue="all"
+          >
             <ToggleGroupItem
               value="all"
-              defaultChecked
               aria-label="Toggle all"
               onClick={handleViewAll}
+              className={"text-lg"}
             >
               All
             </ToggleGroupItem>
@@ -108,6 +130,7 @@ const App: React.FC = () => {
               value="complete"
               aria-label="Toggle Completed"
               onClick={handleViewCompleted}
+              className={"text-lg"}
             >
               Completed
             </ToggleGroupItem>
@@ -115,33 +138,36 @@ const App: React.FC = () => {
               value="incomplete"
               aria-label="Toggle Incomplete"
               onClick={handleViewIncomplete}
+              className={"text-lg"}
             >
               Incomplete
             </ToggleGroupItem>
           </ToggleGroup>
         </div>
-        <div>
-          <ul>
+        <div className="my-2">
+          <ul className="overflow-y-auto max-h-96">
             {segregatedTasks.map((task) => (
               <>
-                <li key={task.id} className="flex flex-row gap-2 rounded border">
-                  <Checkbox
-                    checked={task.complete}
-                    onClick={() => handleComplete(task.id)}
-                    className="rounded-full"
-                  />
-                  <span
-                    className={
-                      task.complete ? "bg-green-100 line-through" : undefined
-                    }
-                  >
-                    {task.content}
-                  </span>
+                <li
+                  key={task.id}
+                  className={`flex flex-row gap-2 rounded border items-center ${
+                    task.complete ? "border-green-300 bg-green-50" : null
+                  } p-2 my-2 justify-between`}
+                >
+                  <div className="space-x-2 items-center flex">
+                    <Checkbox
+                      checked={task.complete}
+                      onClick={() => handleComplete(task.id)}
+                      className={"rounded-full w-6 h-6"}
+                    />
+                    <span className={"text-lg"}>{task.content}</span>
+                  </div>
+
                   <Button
                     variant="ghost"
                     size="icon"
                     onClick={() => handleDelete(task.id)}
-                    className="rounded-full"
+                    className="rounded-full hover:text-red-500"
                   >
                     <RxCross1 />
                   </Button>
@@ -149,15 +175,25 @@ const App: React.FC = () => {
               </>
             ))}
           </ul>
-          <Input
-            placeholder="Type something..."
-            value={task.content}
-            onChange={(e) => setTask({ ...task, content: e.target.value })}
-            required
-          />
-          <Button onClick={handleAddTask} disabled={task.content == ""}>
-            Add task
-          </Button>
+          <div className="space-y-2 my-2">
+            <Input
+              placeholder="Type something (Press Enter to add)"
+              type="text"
+              className={"w-full text-lg"}
+              value={task.content}
+              onChange={handleAddTaskInputChange}
+              onKeyDown={handleKeyPress}
+              required
+              ref={inputRef}
+            />
+            <Button
+              onClick={handleAddTask}
+              disabled={task.content == ""}
+              className={"w-full text-lg"}
+            >
+              Add task
+            </Button>
+          </div>
         </div>
       </div>
     </>
